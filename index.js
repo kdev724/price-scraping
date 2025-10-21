8e5102f5afce6b557d87af64efbdf56c27ba0d3c
tonyrst90 <tonyrst90@hotmail.com>
2025-10-22 08:51:44 +0900

    commit

--- index.js
+++ index.js
@@ -218,7 +218,14 @@ app.post("/initial", async (req, res) => {
 			return res.status(500).json({ error: 'Database not connected. Please try again.' });
 		}
 		
-		await getCanonicalProductId(0)
+		Pedal.countDocuments({
+			priceGuide: { $exists: true, $ne: [] }
+		  }).then(count => {
+			console.log("count", count);
+		  }).catch(err => {
+			console.error(err);
+		  });
+		// await getCanonicalProductId(0)
 
 		// Process existing pedals in batches to verify they are actually guitar pedals
 		// await processExistingPedalsInBatches();
